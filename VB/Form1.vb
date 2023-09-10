1afd1d0b9348d0e96ded5f84a2769e37e619ae08
DevExpressExampleBot <fakeEmail>
2023-09-11 00:18:06 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to bind the ImageComboBoxEdit to a datasource
 ' 
 ' This example demonstrates how the GridLookUpEdit can be customized to look like
@@ -7,64 +7,57 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E2200
-
-
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors.Controls
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Private Function CreateTable() As DataTable
-			Dim tbl As New DataTable()
-			tbl.Columns.Add("Value", GetType(Integer))
-			tbl.Columns.Add("Description", GetType(String))
-			tbl.Columns.Add("Image", GetType(Image))
-			For i As Integer = 0 To imageList1.Images.Count - 1
-				tbl.Rows.Add(New Object(){i, String.Format("Description{0}", i), imageList1.Images(i)})
-			Next i
-			Return tbl
-		End Function
+    Public Partial Class Form1
+        Inherits Form
 
+        Private Function CreateTable() As DataTable
+            Dim tbl As DataTable = New DataTable()
+            tbl.Columns.Add("Value", GetType(Integer))
+            tbl.Columns.Add("Description", GetType(String))
+            tbl.Columns.Add("Image", GetType(Image))
+            For i As Integer = 0 To imageList1.Images.Count - 1
+                tbl.Rows.Add(New Object() {i, String.Format("Description{0}", i), imageList1.Images(i)})
+            Next
 
-		Public Sub New()
-			InitializeComponent()
-			gridLookUpEdit1.Properties.DataSource = CreateTable()
-			gridLookUpEdit1.Properties.DisplayMember = "Description"
-			gridLookUpEdit1.Properties.ValueMember = "Value"
-			AddHandler gridLookUpEdit1.EditValueChanged, AddressOf gridLookUpEdit1_EditValueChanged
-			SyncButtonImage()
-		End Sub
+            Return tbl
+        End Function
 
-		Private Function GetSelectedImage() As Image
-			Dim row As DataRowView = TryCast(gridLookUpEdit1.Properties.GetRowByKeyValue(gridLookUpEdit1.EditValue), DataRowView)
-			If row Is Nothing Then
-				Return Nothing
-			End If
-			Return TryCast(row("Image"), Image)
-		End Function
+        Public Sub New()
+            InitializeComponent()
+            gridLookUpEdit1.Properties.DataSource = CreateTable()
+            gridLookUpEdit1.Properties.DisplayMember = "Description"
+            gridLookUpEdit1.Properties.ValueMember = "Value"
+            AddHandler gridLookUpEdit1.EditValueChanged, New EventHandler(AddressOf gridLookUpEdit1_EditValueChanged)
+            SyncButtonImage()
+        End Sub
 
+        Private Function GetSelectedImage() As Image
+            Dim row As DataRowView = TryCast(gridLookUpEdit1.Properties.GetRowByKeyValue(gridLookUpEdit1.EditValue), DataRowView)
+            If row Is Nothing Then Return Nothing
+            Return TryCast(row("Image"), Image)
+        End Function
 
-		Private Sub SetButtonImage(ByVal selectedImage As Image, ByVal buttonIndex As Integer)
-			Dim button As EditorButton = gridLookUpEdit1.Properties.Buttons(buttonIndex)
-			button.Visible = Not(selectedImage Is Nothing)
-			button.Image = selectedImage
-		End Sub
+        Private Sub SetButtonImage(ByVal selectedImage As Image, ByVal buttonIndex As Integer)
+            Dim button As EditorButton = gridLookUpEdit1.Properties.Buttons(buttonIndex)
+            button.Visible = Not selectedImage Is Nothing
+            button.Image = selectedImage
+        End Sub
 
-		Private Sub SyncButtonImage()
-			SetButtonImage(GetSelectedImage(), 1)
-		End Sub
+        Private Sub SyncButtonImage()
+            SetButtonImage(GetSelectedImage(), 1)
+        End Sub
 
-		Private Sub gridLookUpEdit1_EditValueChanged(ByVal sender As Object, ByVal e As EventArgs)
-			BeginInvoke(New Action(AddressOf SyncButtonImage))
-		End Sub
-	End Class
+        Private Sub gridLookUpEdit1_EditValueChanged(ByVal sender As Object, ByVal e As EventArgs)
+            BeginInvoke(New Action(AddressOf SyncButtonImage))
+        End Sub
+    End Class
 End Namespace
--- VB/Program.vb
+++ VB/Program.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to bind the ImageComboBoxEdit to a datasource
 ' 
 ' This example demonstrates how the GridLookUpEdit can be customized to look like
@@ -7,25 +7,21 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E2200
-
-
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Class
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
